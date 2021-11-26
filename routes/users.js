2cca0e70329c18a6bd184930ec127343d224197a
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2021-11-26 22:36:37 +0200

    Add routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile');
+});
+
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile/:user_id');
 });
 
 module.exports = router;
